be23dc953515db69e5798cd9058cd3a1f793437c
Arianaomi <arianaomi.lp@gmail.com>
2020-08-25 15:08:31 -0500

    :see_no_evil: Add .vscode
    Add .vscode

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/_globals.scss'
+import 'antd/dist/antd.css'
 
-function MyApp ({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
