84712df51f8a1e2401e8b18512a7876a1615ff85
AniaCuapio <ania.cuapio@gmail.com>
2020-08-26 00:41:37 -0500

    routing ready

---
+++ components/Pages/LoginForm/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function LoginForm() {
+  return (<h1>FORM</h1>)
+}
+
+export default LoginForm
--- pages/log-in.js
+++ pages/log-in.js
@@ -1,7 +1,10 @@
-export default function Signup () {
+import LoginForm from '../components/Pages/LoginForm'
+
+export default function Login () {
   return (
     <div>
       <h1>Log-in </h1>
+      <LoginForm />
     </div>
   )
 }
