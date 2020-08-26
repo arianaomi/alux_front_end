43e4455ab9ba2bba97eccf764ee86d8bf5400849
AniaCuapio <ania.cuapio@gmail.com>
2020-08-25 23:46:10 -0500

    login

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import LogIn from './LogIn';
-import Code from '../components/Code';
+import LogIn from './LogIn'
+// import Code from '../components/Code'
 
 export default function Home () {
   return (
     <div>
       <h1>index.js '/'</h1>
       <LogIn />
-      <Code value='https://www.youtube.com/watch?v=xuc9C-C6Ldw' />
+
+      {/* <Code value='https://www.youtube.com/watch?v=xuc9C-C6Ldw' /> */}
     </div>
   )
 }
