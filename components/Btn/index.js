217c59d85844c234cc1fb76aab2cd1478ec3181d
AniaCuapio <ania.cuapio@gmail.com>
2020-08-26 00:16:53 -0500

    buttons ready

--- components/Btn/index.js
+++ components/Btn/index.js
@@ -2,11 +2,13 @@
 import styles from './Btn.module.scss'
 import cx from 'classnames'
 
-function Btn ({ content, color }) {
+function Btn({ content, classProp = "btn_primary" }) {
+
   return (
     <>
       <h1>Button</h1>
-      <button className={cx(styles.btn, styles.btn_primary)}>{color}</button>
+      {/* <button className={cx(styles.btn, { styles[classProp] })}>{content}</button> */}
+      <button className={`${styles.btn} ${styles[classProp]}`}>{content}</button>
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Btn from '../components/Btn'
+// import styles from '../components/Btn'
+
 export default function Home() {
   return (
     <div>
       <h1>index.js '/'</h1>
-      <Btn content='Click !' color='btn_primary' />
+      <Btn content='Click !' />
     </div>
   )
 }
