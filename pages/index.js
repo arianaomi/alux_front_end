81cc4cb0d82b1397069d987e9a7b64ff7b173f87
Arianaomi <arianaomi.lp@gmail.com>
2020-08-26 01:06:28 -0500

    :construction: Routing
    Routing

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-import Btn from '../components/Btn'
-// import styles from '../components/Btn'
-
-export default function Home () {
-  return (
-    <div>
-      <h1>index.js '/'</h1>
-      <Btn content='Click !' typeBtn='btn_alert' />
-=======
 export default function WelcomePage () {
   return (
     <div>
       <h1> Welcome Page </h1>
->>>>>>> develop
     </div>
   )
 }
