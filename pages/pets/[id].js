8d56da9b502be89944d04e9c8dd3fd018d2a1b1c
Ruben Somuano <rubasomuano.g2@gmail.com>
2020-09-01 09:50:50 -0500

    perfil

--- pages/pets/[id].js
+++ pages/pets/[id].js
@@ -1,7 +1,20 @@
+import HeaderRectan from '../../components/HeaderRectan'
+import { Avatar, Row, Col } from 'antd'
 export default function PestId() {
   return (
-    <div>
-      <h1>perfil de una mascota como admin</h1>
-    </div>
+    <>
+      <HeaderRectan />
+      <Row justify="center">
+        <Col xs={24} sm={24} md={22} lg={22} xl={22}>
+
+
+          <div>
+
+            <h1>perfil de una mascota como admi gg  ngibfb kk knl nn onononrovo nononvnvknkvl knñfnvlnlnlvklkndonoignov inoenn</h1>
+
+          </div>
+        </Col>
+      </Row>
+    </>
   )
 }
