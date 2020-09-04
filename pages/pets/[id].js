afcec4d2385ee019ad91b71a8da60a66bfc3d75b
Arianaomi <arianaomi.lp@gmail.com>
2020-09-04 05:41:48 -0500

    :zap: perfil
    perfil

--- pages/pets/[id].js
+++ pages/pets/[id].js
@@ -1,34 +1,73 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import HeaderRectan from '../../components/HeaderRectan'
 import { Row, Col, Form, Input, InputNumber, Button } from 'antd'
 import Footer from '../../components/Footer'
 import AvatarCmptn from '../../components/AvatarCmpnt'
 import CardGralxl from '../../components/cardGeneralXL'
 import styles from '../../styles/_PetProfile.module.scss'
+import CardPost from '../../components/cardGeneralXL'
+import Layout from '../../components/Layout'
+import { getPetIdService, getPostsService } from '../../services'
 /*{ title, image, namePet, text }*/
 export default function PestId(imagePet) {
+  const router = useRouter()
+  const [petId, setPetId] = useState('')
+  const [post, setPost] = useState([])
+
+  useEffect(() => {
+    const postId = router.query.id
+    const token = localStorage.getItem('token')
+    const owner = localStorage.getItem('userId')
+    if (postId) {
+      ;(async () => {
+        try {
+          const { data } = await getPetIdService(token, postId)
+          let pet = data.pet
+          setPetId(pet)
+
+          const {
+            data: { entries },
+          } = await getPostsService()
+          let content = entries.filter(user => owner === user.user)
+          //console.log(content)
+
+          setPost(content)
+
+          //console.log(entries)
+        } catch (error) {
+          console.log(error)
+        }
+      })()
+    }
+  }, [router.query.id])
+
+  let content = post.filter(user => user === post.user)
+  console.log(content)
+  const UICardsPost = post.map(({ title, imageurl, user, content, _id }) => (
+    <Col xs={22} md={10} lg={10} className={styles.post} key={_id}>
+      <CardPost
+        title={title}
+        image={imageurl}
+        namePet={user.name}
+        text={content}
+      />
+    </Col>
+  ))
   return (
-    <>
-      <Row>
-        <Col xs={24}>
-          <HeaderRectan />
-        </Col>
-      </Row>
+    <Layout title={`Perfil de ${petId.name}`}>
       <Row justify='center'>
         <Col xs={24} sm={24} md={22} lg={19} xl={19}>
           <div className={styles.continerpicProfilePet}>
             <div className={styles.picProfilePet}>
               <div className={styles.containerAv}>
-                <AvatarCmptn src={imagePet} />
+                <AvatarCmptn size='mdd' />
               </div>
               <div className={styles.containerNom}>
-                <p className={styles.NomPet}>Luno</p>
+                <p className={styles.NomPet}>{petId.name}</p>
               </div>
               <div className={styles.continerTextProfilePet}>
-                <p className={styles.textDescPet}>
-                  Soy un gatito muy travieso, me gusta salir con los gatitos de
-                  barrio, con mi novia y darnos unos besos. Noy fan de la leche
-                  y mis laitas de carne. Pronto conquistaré el mundo.
-                </p>
+                <p className={styles.textDescPet}>{petId.about}</p>
               </div>
             </div>
           </div>
@@ -62,27 +101,9 @@ export default function PestId(imagePet) {
               />
             </div>
           </div>
-          <div className={styles.containerCards}>
-            <div className={styles.containerUnG}>
-              <CardGralxl />
-              <CardGralxl />
-            </div>
-            <div className={styles.containerUnG}>
-              <CardGralxl />
-              <CardGralxl />
-            </div>
-            <div className={styles.containerUnG}>
-              <CardGralxl />
-              <CardGralxl />
-            </div>
-          </div>
         </Col>
       </Row>
-      <Row>
-        <Col xs={24}>
-          <Footer />
-        </Col>
-      </Row>
-    </>
+      <Row justify='center'>{UICardsPost}</Row>
+    </Layout>
   )
 }
